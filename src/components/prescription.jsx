e2658b127b09d94333a90d86a862abce2417514c
Suciu Andrei <andrei.suciu21@yahoo.com>
2023-04-06 22:04:55 +0300

    Implementare form pentru data, simptome și tratament.

--- src/components/prescription.jsx
+++ src/components/prescription.jsx
@@ -6,20 +6,34 @@ const Prescription = ()=>{
         <div style={{display: "flex", flexDirection:"column", width:"100%", alignItems:"center"}}> 
             <div style={{ fontWeight: "1000", fontSize: "25px"}}>Prescriptie Medicala</div>
             <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "50px"}}>
-                <div style={{marginRight: "20px"}}>Nume</div><input id="nume"/>
+                <div style={{marginRight: "20px"}}>Nume:</div><input id="nume"/>
             </div>
             <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "20px"}}>
-                <div style={{marginRight: "20px"}}>Varsta pacientului</div><input type="number" id="varsta"/>
+                <div style={{marginRight: "20px"}}>Varsta pacientului:</div><input type="number" id="varsta"/>
             </div>
             <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "20px"}}>
-                <div style={{marginRight: "20px"}}>Sexul pacientului</div>
+                <div style={{marginRight: "20px"}}>Sexul pacientului:</div>
                 <select id="Pacient">
                     <option value="masculin">Masculin</option>
                     <option value="feminin">Feminin</option>
                 </select>
             </div>
+            <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "20px"}}>
+                <div style={{marginRight: "20px"}}>Data:</div><input type="date" id="data"/>
+            </div>
+            <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "20px"}}>
+                <div>Simptome asociate pacientului:
+                    <textarea id="simptome" rows={4} cols={50}/>
+                </div>
+            </div>
+            <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "20px"}}>
+                <div>Tratament si medicatie recomandata:
+                    <textarea id="tratament" rows={4} cols={50}/>
+                </div>
+            </div>
         </div>
     </div>
     );
+    
 }
 export default Prescription;
