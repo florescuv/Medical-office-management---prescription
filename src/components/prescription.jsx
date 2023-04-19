545493da5faadc9404333440c004e68899e62ba0
Suciu Andrei <andrei.suciu21@yahoo.com>
2023-04-20 00:03:10 +0300

    Refactorizare majora a formularului

--- src/components/prescription.jsx
+++ src/components/prescription.jsx
@@ -1,37 +1,169 @@
-import React from 'react';
+import { Button, Form, Input, Select, DatePicker, Layout } from "antd";
+import React from "react";
+const { Option } = Select;
+const { TextArea } = Input;
+const { Header, Content } = Layout;
 
-const Prescription = ()=>{
-    return(
-    <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginBottom:"20px"}}>
-        <div style={{display: "flex", flexDirection:"column", width:"100%", alignItems:"center"}}> 
-            <div style={{height:"70px", fontWeight: "1000", fontSize: "40px", backgroundColor: "#247BA0", width:"100vw", textAlign:"center"}}>Prescriptie Medicala</div>
-            <div style={{display: "flex", flexDirection:"row", justifyContent: "space-between", marginTop: "50px", width: "600px"}}>
-                <div style={{marginRight: "20px"}}>Nume:</div><input style={{width: "400px", backgroundColor: "#F3FFBD"}}id="nume"/>
-            </div>
-            <div style={{display: "flex", flexDirection:"row",  justifyContent: "space-between", marginTop: "20px"}}>
-                <div style={{marginRight: "56px"}}>Varsta pacientului:</div><input style={{width: "400px", backgroundColor: "#F3FFBD"}} type="number" id="varsta"/>
-            </div>
-            <div style={{display: "flex", flexDirection:"row", justifyContent: "space-between", marginTop: "20px"}}>
-                <div style={{marginRight: "63px"}}>Sexul pacientului:</div>
-                <select style={{width: "410px", backgroundColor: "#F3FFBD"}} id="Pacient">
-                    <option value="masculin">Masculin</option>
-                    <option value="feminin">Feminin</option>
-                </select>
-            </div>
-            <div style={{display: "flex", flexDirection:"row", justifyContent: "space-between", alignItems: "center", marginTop: "20px"}}>
-                <div style={{marginRight: "153px"}}>Data:</div><input style={{width: "408px", backgroundColor: "#F3FFBD"}} type="date" id="data"/>
-            </div>
-            <div style={{display: "flex", flexDirection:"row", alignContent:"center", height: "100%", alignItems: "center", marginTop: "20px"}}>
-                <div style={{marginRight: "43px", width:"150px"}}>Simptome asociate pacientului:</div>
-                    <textarea style={{width: "405px", backgroundColor: "#F3FFBD"}} id="simptome" rows={4} cols={50}/>
-            </div>
-            <div style={{display: "flex", flexDirection:"row", alignItems: "center", marginTop: "20px"}}>
-                <div style={{marginRight: "43px", width:"150px"}}>Tratament si medicatie recomandata:</div>
-                <textarea style={{width: "405px", backgroundColor: "#F3FFBD"}} id="tratament" rows={4} cols={50}/>
-            </div>
-        </div>
-    </div>
-    );
-    
-}
-export default Prescription;
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 2,
+    },
+    sm: {
+      span: 4,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const config = {
+  rules: [
+    {
+      type: "object",
+      required: true,
+      message: "Please select time!",
+    },
+  ],
+};
+
+const PrescriptionForm = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    const url = new URL(window.location.origin);
+    const data = {
+      ...values,
+      date: values.date.format("DD-MM-YYYY"),
+    };
+    Object.keys(data).forEach((key) => {
+      data[key] = JSON.stringify(data[key]);
+    });
+    url.search = new URLSearchParams(data);
+    window.open(url, "_blank").focus();
+  };
+
+  return (
+    <Layout>
+      <Header
+        style={{
+          color: "#fff",
+          textAlign: "center",
+          fontWeight: "bolder",
+          fontSize: "2rem",
+        }}
+      >
+        Doctor Prescription Generator
+      </Header>
+      <Content>
+        <Form
+          style={{ padding: "3rem" }}
+          {...formItemLayout}
+          form={form}
+          name="register"
+          onFinish={onFinish}
+          scrollToFirstError
+        >
+          <Form.Item
+            name="username"
+            label="Patient Name"
+            rules={[
+              {
+                type: "uername",
+                message: "The input is not valid username!",
+              },
+              {
+                required: true,
+                message: "Please input your username",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Patient's Age"
+            name="age"
+            rules={[{ required: true }]}
+          >
+            <Input type={"number"} name="age" />
+          </Form.Item>
+
+          <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+            <Select
+              placeholder="Select a option and change input text above"
+              allowClear
+            >
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+              <Option value="other">other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            noStyle
+            shouldUpdate={(prevValues, currentValues) =>
+              prevValues.gender !== currentValues.gender
+            }
+          >
+            {({ getFieldValue }) =>
+              getFieldValue("gender") === "other" ? (
+                <Form.Item
+                  name="customizeGender"
+                  label="Customize Gender"
+                  rules={[{ required: true }]}
+                >
+                  <Input />
+                </Form.Item>
+              ) : null
+            }
+          </Form.Item>
+
+          <Form.Item name="date" label="DatePicker" {...config}>
+            <DatePicker />
+          </Form.Item>
+
+          <Form.Item
+            label="Medical Notes"
+            name="notes"
+            rules={[{ required: true }]}
+          >
+            <TextArea rows={4} name="notes" />
+          </Form.Item>
+
+          <Form.Item
+            label="Details Of Medicines"
+            name="medicines"
+            rules={[{ required: true }]}
+          >
+            <TextArea rows={4} name="medicines" />
+          </Form.Item>
+
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Generate Prescription
+            </Button>
+          </Form.Item>
+        </Form>
+      </Content>
+    </Layout>
+  );
+};
+
+export default PrescriptionForm;
