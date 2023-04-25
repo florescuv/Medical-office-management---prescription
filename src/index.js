2021f37825d16bc13420919902e1ec23e4130a19
Radocea-Catalin <catalin.radocea05@yahoo.com>
2023-04-25 17:22:56 +0300

    Editarea culorilor formularului pentru o mai buna vizualizare a acestuia.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div style={{backgroundColor:"#B2DBBF"}}>
+    <div style={{backgroundColor:"white"}}>
       <App />
     </div>
   </React.StrictMode>
