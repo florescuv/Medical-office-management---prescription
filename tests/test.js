97647956c854f79c65bdf7f7fe4244f3f000341d
RaduAndronic <100286948+RaduAndronic@users.noreply.github.com>
2023-04-26 23:20:12 +0300

    Testare data reteta

--- tests/test.js
+++ tests/test.js
@@ -12,6 +12,8 @@ test('Fill form and validate Result', async t => {
 		.wait(1000)
 		.click('body > div:nth-child(3) > div > div > div.rc-virtual-list > div.rc-virtual-list-holder > div > div > div:nth-child(1)')
 		.wait(3000)
+		.typeText('#register_date', '2023-04-05')
+		.wait(3000)
 }).timeouts({
     pageLoadTimeout:    3000,
 });
